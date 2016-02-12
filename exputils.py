53ccd6beddb119658b6286415a7f37d1fa74f89a
mmagnuski <mmagnuski@swps.edu.pl>
2016-02-12 22:27:35 +0100

    add fixation def

--- exputils.py
+++ exputils.py
@@ -135,6 +135,23 @@ class SternbergExperiment(object):
 		self.df.to_excel(full_path + '.xls')
 
 
+# stimuli
+# -------
+def fix(win, height=0.3, width=0.1, shape='circle', color=(0.5, 0.5, 0.5)):
+	args = {'fillColor': color, 'lineColor': color,
+		'interpolate': True, 'units': 'deg'}
+	if shape == 'circle':
+		fix_stim = visual.Circle(win, radius=height/2,
+			edges=32, **args)
+	else:
+		h, w = (height/2, width/2)
+		vert = np.array([[w, -h], [w, h], [-w, h], [-w, -h]])
+
+		args.update(closeShape=True)
+		fix_stim = [visual.ShapeStim(win, vertices=v, **args)
+					for v in [vert, np.fliplr(vert)]]
+	return fix_stim
+
 def get_frame_time(win, frames=25):
 	frame_rate = win.getActualFrameRate(nIdentical = frames)
 	return 1.0 / frame_rate
