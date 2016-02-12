b187217dc66e9d28b56d458f6f67654bbae1709f
mmagnuski <mmagnuski@swps.edu.pl>
2016-02-12 22:30:34 +0100

    change test

--- turbo_sternberg_simpletest.py
+++ turbo_sternberg_simpletest.py
@@ -9,12 +9,19 @@ from exputils import (SternbergExperiment,
 window = visual.Window(monitor="testMonitor", units="deg")
 exp = SternbergExperiment(window, "parameters.yaml")
 
+print(exp.df.loc[0:5,:])
+print(exp.times)
+print(exp.resp_keys)
+print(exp.resp_mapping)
+print(exp.df.dtypes)
+
 # run one trial:
-exp.show_trial(0)
-core.wait(0.5)
+#exp.show_trial(0)
+#core.wait(0.5)
 
 # run more:
 exp.run_trials(range(1,4))
 
-pth = r'E:\Programy\EXP\TurboSternberg\data'
+pth = r'E:\PROJ\EXP\TurboSternberg\data'
 exp.save_data(pth)
+#print(exp.df.loc[0:5,:])
